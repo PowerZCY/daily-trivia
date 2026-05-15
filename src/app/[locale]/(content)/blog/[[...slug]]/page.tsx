56be1057e3948a1e940bd8b09508e2b71c373df4
D8GER <xyb5to0zcy@gmail.com>
2026-05-15 14:19:08 +0800

    feat(dependencies): update @windrun-huaiin/lib to version 31.0.2 and @windrun-huaiin/third-ui to version 31.3.1; refactor sitemap implementation to use resolveMdxSourceDir function

--- src/app/[locale]/(content)/blog/[[...slug]]/page.tsx
+++ src/app/[locale]/(content)/blog/[[...slug]]/page.tsx
@@ -7,7 +7,6 @@ const { Page, generateStaticParams, generateMetadata } = createFumaPage({
   sourceKey: sourceKey,
   mdxContentSource: () => siteDocs.getContentSource(sourceKey),
   getMDXComponents: siteDocs.getMDXComponents,
-  mdxSourceDir: appConfig.mdxSourceDir[sourceKey],
   githubBaseUrl: appConfig.githubBaseUrl,
   showBreadcrumb: false,
   showTableOfContent: true,
